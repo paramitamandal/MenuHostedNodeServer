954c4b50e0bafd952af77e15d6c889f22da521af
paramitamandal <paramita.mandal@hyland.com>
2018-09-28 11:36:01 +0530

    node modules added

--- server.js
+++ server.js
@@ -78,52 +78,14 @@ app.get("/api/getVideo", function(req , res){
         req.query(query).then(function (recordset) {
             console.log(recordset);
             conn.close();
-
-            // var videoFile;
-            // fs.readFile(path.resolve(__dirname,"1.mp4"), function (err, data) {
-            //     if (err) {
-            //         throw err;
-            //     }
-            //     videoFile = data;
-            // });
-
-            // var range = req.headers.range;
-            // var positions = range.replace(/bytes=/, "").split("-");
-            // var start = parseInt(positions[0], 10);
-            // var end = partialend ? parseInt(partialend, 10) : total - 1;
-            // var chunksize = (end-start)+1;
-            // var total = videoFile.length;
-            // console.log(total);
-
-            // res.writeHead(206, { "Content-Range": "bytes " + start + "-" + end + "/" + total, 
-            //                         "Accept-Ranges": "bytes",
-            //                         "Content-Length": chunksize,
-            //                         "Content-Type":"video/mp4"});
-            // res.end(videoFile.slice(start, end+1), "binary");
-            
+        
             var path = 'videos/1.mp4';
             var stat = fs.statSync(path);
             var total = stat.size;
-            // if (req.headers['range']) {
-            //   var range = req.headers.range;
-            //   var parts = range.replace(/bytes=/, "").split("-");
-            //   var partialstart = parts[0];
-            //   var partialend = parts[1];
-          
-            //   var start = parseInt(partialstart, 10);
-            //   var end = partialend ? parseInt(partialend, 10) : total-1;
-            //   var chunksize = (end-start)+1;
-            //   console.log('RANGE: ' + start + ' - ' + end + ' = ' + chunksize);
-          
-            //   var file = fs.createReadStream(path, {start: start, end: end});
-            //   res.writeHead(206, { 'Content-Range': 'bytes ' + start + '-' + end + '/' + total, 'Accept-Ranges': 'bytes', 'Content-Length': chunksize, 'Content-Type': 'video/mp4' });
-            //   file.pipe(res);
-            // } else {
-              console.log('ALL: ' + total);
-              res.writeHead(200, { 'Content-Length': total, 'Content-Type': 'video/mp4' });
-              fs.createReadStream(path).pipe(res);
-            // }
-
+           
+            console.log('ALL: ' + total);
+            res.writeHead(200, { 'Content-Length': total, 'Content-Type': 'video/mp4' });
+            fs.createReadStream(path).pipe(res);
 
         })
         .catch(function (err) {
